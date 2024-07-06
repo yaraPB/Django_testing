35284aa013737ac4f2f5994ac638ec89a7fb6999
Yara PB <yarakouttane234@gmail.com>
2024-07-06 23:45:33 +0100

    learned more about the Django webapp and how to manage posts and applications

--- djangoTesting/posts/views.py
+++ djangoTesting/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', { 'posts': posts})
     
