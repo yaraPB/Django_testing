22191a1b759b7f04b7f12f7744fcf31a20e51daa
Yara PB <yarakouttane234@gmail.com>
2024-07-07 11:43:03 +0100

    learning more about adding pages and how to include images and stuff

--- djangoTesting/posts/urls.py
+++ djangoTesting/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.post_list),
+    path('', views.post_list, name="list"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
