7dce127dc6cde3f053550bbbc60914875c28b768
Yara PB <yarakouttane234@gmail.com>
2024-07-07 21:21:52 +0100

    worked on user authorization

--- djangoTesting/posts/views.py
+++ djangoTesting/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +10,7 @@ def post_list(request):
 def post_page(request, slug): 
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', { 'post': post})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
--- djangoTesting/users/views.py
+++ djangoTesting/users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request,  form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
@@ -18,7 +18,15 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             #LOGIN HERE
-            return redirect("posts:list")
+            login(request, form.get_user())
+            if 'next' in request.POST:
+                return redirect("posts:list")
     else: 
         form = AuthenticationForm()
     return render(request, "users/login.html", { "form": form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
+    
